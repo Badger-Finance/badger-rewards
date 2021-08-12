a777f0360e80a2744bee3a30f07e983d61346a28
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-08-12 13:18:17 -0400

    initial cicd commit, add k8s files

--- config/env_config.py
+++ config/env_config.py
@@ -1,13 +1,12 @@
+from rewards.aws.helpers import get_secret
 import os
 
 
 class EnvConfig:
     def __init__(self):
-        self.aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID", '')
-        self.aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY", '')
-        self.graph_api_key = os.getenv("GRAPH_API_KEY", '')
-        self.test_webhook_url = os.getenv("TEST_WEBHOOK_URL", '')
-        self.discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL", '')
+        self.graph_api_key = get_secret("GRAPH_API_KEY")
+        self.test_webhook_url = get_secret("TEST_WEBHOOK_URL")
+        self.discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL", "") # get_secret("DISCORD_WEBHOOK_URL", '')
         self.test = os.getenv("TEST", False).lower() in ['true', '1', 't', 'y', 'yes']
 
     def get_webhook_url(self):
