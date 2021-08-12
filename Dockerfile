d7c29fd680e5cb78a1567981082a7332ec5eafc4
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-08-12 19:06:20 -0400

    test new start script

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM python:3.9
 
 WORKDIR /boost
 
+RUN chmod 755 start.sh
 COPY requirements.txt .
+RUN apt-get update
+RUN apt-get install -y jq && apt-get install -y awscli
 RUN pip install -r requirements.txt
 
 COPY . .
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws configure set region us-west-1
+export WEB3_INFURA_PROJECT_ID=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-west-1:342684350154:secret:boost-bot/infura-id-zb9i9o | jq -r ".SecretString" | jq -r ".INFURA_ID")
+brownie run scripts/rewards/propose_boost.py --network=mainnet
