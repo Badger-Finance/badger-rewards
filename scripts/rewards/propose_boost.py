088e63bdcefc666275780c7b395032bb335ada2e
mitche50 <mitche50@gmail.com>
2021-08-13 20:07:53 -0400

    chore: remove unused

--- scripts/rewards/propose_boost.py
+++ scripts/rewards/propose_boost.py
@@ -1,10 +1,8 @@
 from config.env_config import env_config
 from helpers.discord import send_message_to_discord
-from rewards.aws.helpers import get_secret
 from rewards.boost.calc_boost import badger_boost
 from rewards.aws.boost import add_user_data
 from rich.console import Console
-from web3.auto.infura import w3
 
 console = Console()
 
