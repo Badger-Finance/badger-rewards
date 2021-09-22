ef26a751f15c31187b5b936c5d4a4b87e5485d48
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-09-22 14:40:24 -0400

    add a few tests

--- tests/test_tree_utils.py
+++ tests/test_tree_utils.py
@@ -1,11 +1,68 @@
 import os
 import pytest
+import json
 from brownie import accounts, Contract, web3
+from eth_account import Account
 from decimal import Decimal
 from hexbytes import HexBytes
 from web3 import contract
 
 
-from rewards.tree_utils import get_last_proposed_cycle
+from rewards.tree_utils import get_last_proposed_cycle, calc_next_cycle_range
 from rewards.classes.TreeManager import TreeManager
-from tests.utils import test_address, test_key
+from tests.utils import test_address, test_key, chains, mock_tree
+
+
+def mock_download_tree(file_name: str, chain: str):
+    return json.dumps(mock_tree)
+
+
+def mock_validate_tree(merkle, tree):
+    return True
+
+
+@pytest.fixture(autouse=True)
+def mock_fns(monkeypatch):
+    monkeypatch.setattr("rewards.classes.TreeManager.download_tree", mock_download_tree)
+
+
+@pytest.fixture
+def cycle_key() -> str:
+    return test_key
+
+
+@pytest.fixture
+def tree_manager(cycle_key, request) -> TreeManager:
+    tree_manager = TreeManager(request.param, Account.from_key(cycle_key))
+    tree_manager.validate_tree = mock_validate_tree
+    return tree_manager
+
+
+@pytest.mark.parametrize(
+    "tree_manager",
+    chains,
+    indirect=True,
+)
+def test_get_last_proposed_cycle(tree_manager):
+    if tree_manager.has_pending_root():
+        rewards = tree_manager.fetch_current_tree()
+        claim_end = tree_manager.last_propose_end_block()
+        claim_start = tree_manager.last_propose_start_block()
+        assert get_last_proposed_cycle(tree_manager.chain, tree_manager) == (
+            rewards,
+            claim_start,
+            claim_end,
+        )
+    else:
+        assert get_last_proposed_cycle(tree_manager.chain, tree_manager) == ({}, 0, 0)
+
+
+@pytest.mark.parametrize(
+    "tree_manager",
+    chains,
+    indirect=True,
+)
+def test_calc_next_cycle_range(tree_manager):
+    result = calc_next_cycle_range(tree_manager.chain, tree_manager)
+
+    assert result[0] == mock_tree
--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,13 @@
 from brownie import Contract
 from decimal import Decimal
 from hexbytes import HexBytes
+import json
 
 test_address = "0xD88a9aF149366d57aEbc32D2eABAdf93EdA41A84"
 test_key = "0f0bdc830bde4be43c3a54c369c6f6a94ac9071911dc3913e35ce5ed8fe955b9"
+chains = ["eth", "arbitrum", "polygon"]
+with open("./tests/mock_tree.json") as f:
+    mock_tree = json.load(f)
 
 
 def mock_send_discord(
