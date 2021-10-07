68a5b35d79d9f574167eb3685f16a77cec1286b5
Darruma <umerderr@gmail.com>
2021-10-07 20:06:35 +0100

    feat: change propose time

--- scripts/rewards/retro_eth_approve.py
+++ scripts/rewards/retro_eth_approve.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     chain = "eth"
     tree_file_name = "rewards-1-0x83b8544a0ea1cac9747c4aec3c9e6df79611bd6c1e54333d101ac162df82cd91.json"
     tree = json.load(open(tree_file_name))
-    start_block = tree["endBlock"] + 1
+    start_block = int(tree["endBlock"]) + 1
     
     with open(config("KEYFILE")) as key_file:
         key_file_json = json.load(key_file)
