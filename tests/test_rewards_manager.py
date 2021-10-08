67b83c40c8b0af04e50f5dd83e91f402e84a7706
Darruma <umerderr@gmail.com>
2021-10-08 19:13:02 +0100

    feat: fix tests

--- tests/test_rewards_manager.py
+++ tests/test_rewards_manager.py
@@ -1,12 +1,13 @@
 import os
 import pytest
 import json
-from rewards.classes.RewardsManager import RewardsManager
 from tests.utils import mock_boosts, test_cycle, test_start, test_end
 from unittest import TestCase
 
 os.environ["KUBE"] = "False"
 os.environ["TEST"] = "True"
+
+from rewards.classes.RewardsManager import RewardsManager
 
 
 @pytest.fixture
@@ -27,15 +28,14 @@ def end() -> bool:
 @pytest.fixture
 def boosts():
     return mock_boosts
-
-
+    
 def mock_get_sett_multipliers():
     return mock_boosts["multiplierData"]
 
 
 @pytest.fixture
 def rewards_manager(cycle, start, end, boosts, request) -> RewardsManager:
-    rewards_manager = RewardsManager(request.param, cycle, start, end, boosts)
+    rewards_manager = RewardsManager(request.param, cycle, start, end, boosts["userData"])
     rewards_manager.get_sett_multipliers = mock_get_sett_multipliers
     return rewards_manager
 
@@ -48,5 +48,6 @@ def rewards_manager(cycle, start, end, boosts, request) -> RewardsManager:
 def test_get_user_multipliers(rewards_manager: RewardsManager, boosts):
     user_multipliers = rewards_manager.get_user_multipliers()
     for user, data in user_multipliers.items():
-        user_info = boosts["userData"][user]
-        TestCase().assertDictEqual(data, user_info)
+        for sett, mult in data.items():
+            if sett in boosts["userData"][user]["multipliers"]:
+                assert mult == boosts["userData"][user]["multipliers"][sett]
