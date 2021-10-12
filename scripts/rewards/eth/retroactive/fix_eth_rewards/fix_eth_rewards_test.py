b1506dfde926ac866801e8f4e07bf01d24f90935
Darruma <umerderr@gmail.com>
2021-10-12 16:16:04 +0100

    feat: add test script for retroactive

---
+++ scripts/rewards/eth/retroactive/fix_eth_rewards/fix_eth_rewards_test.py
@@ -0,0 +1,21 @@
+from config.env_config import env_config
+from rewards.classes.TreeManager import TreeManager
+from rewards.aws.helpers import get_secret
+from eth_account import Account
+from scripts.rewards.eth.retroactive.fix_eth_rewards.fix_eth_rewards import (
+    fix_eth_rewards,
+)
+
+
+if __name__ == "__main__":
+    chain = "eth"
+    cycle_key = get_secret(
+        "arn:aws:secretsmanager:us-west-1:747584148381:secret:/botsquad/cycle_0/private",
+        "private",
+        assume_role_arn="arn:aws:iam::747584148381:role/cycle20210908001427790200000001",
+        kube=env_config.kube,
+    )
+    cycle_account = Account.from_key(cycle_key)
+
+    tree_manager = TreeManager(chain, cycle_account)
+    rewards = fix_eth_rewards()
