6232b1590099af886a484b62101bad5bc39456c3
Darruma <umerderr@gmail.com>
2021-10-12 16:22:01 +0100

    feat: change test script to load in key locally

--- scripts/rewards/eth/retroactive/fix_eth_rewards/fix_eth_rewards_test.py
+++ scripts/rewards/eth/retroactive/fix_eth_rewards/fix_eth_rewards_test.py
@@ -1,21 +1,27 @@
-from config.env_config import env_config
+from rewards.aws.trees import upload_tree
+from rewards.aws.boost import add_multipliers
 from rewards.classes.TreeManager import TreeManager
+from decouple import config
+import json
 from rewards.aws.helpers import get_secret
 from eth_account import Account
 from scripts.rewards.eth.retroactive.fix_eth_rewards.fix_eth_rewards import (
     fix_eth_rewards,
 )
 
-
 if __name__ == "__main__":
     chain = "eth"
-    cycle_key = get_secret(
-        "arn:aws:secretsmanager:us-west-1:747584148381:secret:/botsquad/cycle_0/private",
-        "private",
-        assume_role_arn="arn:aws:iam::747584148381:role/cycle20210908001427790200000001",
-        kube=env_config.kube,
-    )
-    cycle_account = Account.from_key(cycle_key)
+    with open(config("KEYFILE")) as key_file:
+        key_file_json = json.load(key_file)
 
+    key_decrypt_password = get_secret(
+        config("DECRYPT_PASSWORD_ARN"),
+        config("DECRYPT_PASSWORD_KEY"),
+        region_name="us-west-2",
+    )
+    cycle_key = Account.decrypt(key_file_json, key_decrypt_password)
+    cycle_account = Account.from_key(cycle_key)
     tree_manager = TreeManager(chain, cycle_account)
+
     rewards = fix_eth_rewards()
+  
