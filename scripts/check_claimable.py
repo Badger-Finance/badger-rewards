1e0c11813ba2c5c4ab7403c917ca2e783eba348c
Darruma <umerderr@gmail.com>
2021-10-15 17:03:18 +0100

    feat: fix claims snapshot

---
+++ scripts/check_claimable.py
@@ -0,0 +1,10 @@
+from rewards.snapshot.claims_snapshot import claims_snapshot
+
+
+def print_claimable(chain):
+    snapshots = claims_snapshot(chain)
+    for token, snapshot in snapshots.items():
+        print(token, snapshot.total_balance())
+
+if __name__ == "__main__":
+    print_claimable("arbitrum")
