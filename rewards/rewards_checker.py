dbb7ad234e269e19c2700c2861ee83cbe0fde703
Darruma <30578602+Darruma@users.noreply.github.com>
2021-11-01 15:15:06 +0000

    feat: check subset of total claims (#171)
    * feat: check subset of total claims
    
    * feat: renable rewards checker and change error message
    
    * chore: remove sanity diff param from diff table
    
    * feat: use claims snapshot to check for negative claimable balances
    
    * chore: black formatting
    
    * fix: fix duplicate return

--- rewards/rewards_checker.py
+++ rewards/rewards_checker.py
@@ -1,11 +1,12 @@
 from rewards.classes.TreeManager import TreeManager
-from rewards.tree_utils import calc_claimable_balances
+from rewards.snapshot.claims_snapshot import claims_snapshot
 from tabulate import tabulate
 from rich.console import Console
 from config.env_config import env_config
 from helpers.constants import SANITY_TOKEN_AMOUNT, TOKENS_TO_CHECK
 from helpers.discord import send_code_block_to_discord, send_error_to_discord
 from helpers.digg_utils import digg_utils
+import json
 
 console = Console()
 
@@ -47,16 +48,26 @@ def token_diff_table(name, before, after, decimals=18):
 
 def verify_rewards(past_tree, new_tree, tree_manager: TreeManager, chain: str):
     console.log("Verifying Rewards ... \n")
-
-    claimable_balances = calc_claimable_balances(
-        tree_manager, new_tree["tokenTotals"].keys(), new_tree
-    )
-    for addr, bals in claimable_balances.items():
-        for token, amount in bals.items():
-            assert amount >= 0
+    claim_snapshot = claims_snapshot(chain)
+    negative_claimable = []
+    for token, snapshot in claim_snapshot.items():
+        for addr, amount in snapshot:
+            if amount < 0:
+                if addr not in negative_claimable:
+                    negative_claimable[addr] = {}
+                negative_claimable[addr][token] = amount
+    try:
+        assert len(negative_claimable) == 0
+    except AssertionError as e:
+        send_error_to_discord(
+            e,
+            f"Negative Claimable \n ```{json.dumps(negative_claimable,indent=4)}```",
+            "Negative Rewards Error",
+        )
+        raise e
 
     for name, token in TOKENS_TO_CHECK[chain].items():
-
+      
         total_before_token = int(past_tree["tokenTotals"].get(token, 0))
         total_after_token = int(new_tree["tokenTotals"].get(token, 0))
         console.log(name, total_before_token, total_after_token)
---
+++ scripts/rewards/utils/managers.py
@@ -0,0 +1,16 @@
+from eth_account import Account
+from rewards.aws.helpers import get_secret
+from rewards.classes.TreeManager import TreeManager
+from config.env_config import env_config
+
+
+def get_tree_manager(chain: str):
+    cycle_key = get_secret(
+        "arn:aws:secretsmanager:us-west-1:747584148381:secret:/botsquad/cycle_0/private",
+        "private",
+        assume_role_arn="arn:aws:iam::747584148381:role/cycle20210908001427790200000001",
+        kube=env_config.kube,
+    )
+    cycle_account = Account.from_key(cycle_key)
+
+    return TreeManager(chain, cycle_account)
