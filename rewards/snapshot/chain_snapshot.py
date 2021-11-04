5aae253c96ec108b20d001d6f7554e8a95d2e203
Darruma <umerderr@gmail.com>
2021-11-04 08:59:00 +0000

    feat: remove pro-rata vaults when calculating boost

--- rewards/snapshot/chain_snapshot.py
+++ rewards/snapshot/chain_snapshot.py
@@ -5,6 +5,7 @@ from helpers.constants import (
     SETT_INFO,
     DISABLED_VAULTS,
     EMISSIONS_BLACKLIST,
+    PRO_RATA_VAULTS,
 )
 from helpers.web3_utils import make_contract
 from subgraph.queries.setts import fetch_chain_balances, fetch_sett_balances
@@ -92,7 +93,7 @@ def chain_snapshot_usd(chain: str, block: int) -> Tuple[Counter, Counter]:
     native = Counter()
     non_native = Counter()
     for sett, snapshot in total_snapshot.items():
-        if sett in DISABLED_VAULTS:
+        if sett in [*DISABLED_VAULTS, *PRO_RATA_VAULTS]:
             continue
         usd_snapshot = snapshot.convert_to_usd()
         balances = Counter(usd_snapshot.balances)
