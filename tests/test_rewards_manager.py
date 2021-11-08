7f6bbef9662746b35b3d8f3f43c8fc92f09dcaeb
Darruma <umerderr@gmail.com>
2021-11-08 16:34:45 +0000

    feat: add test for boosting setts

--- tests/test_rewards_manager.py
+++ tests/test_rewards_manager.py
@@ -1,6 +1,4 @@
-import os
 import pytest
-import json
 from rewards.classes.Snapshot import Snapshot
 from tests.utils import mock_boosts, test_cycle, test_start, test_end, set_env_vars, mock_balances
 from unittest import TestCase
@@ -54,13 +52,12 @@ def rewards_manager(cycle, start, end, boosts, request) -> RewardsManager:
 def test_boost_sett(rewards_manager: RewardsManager, balances):
     sett = "0xd04c48A53c111300aD41190D63681ed3dAd998eC"
     boosted_bals = {
-        "0xaffb3b889E48745Ce16E90433A61f4bCb95692Fd": 2000,
+        "0xaffb3b889E48745Ce16E90433A61f4bCb95692Fd": 200000,
         "0xbC641f6C6957096857358Cc70df3623715A2ae45": 50000,
-        "0xA300a5816A53bb7e256f98bf31Cb1FE9a4bbcAf0": 3712000
+        "0xA300a5816A53bb7e256f98bf31Cb1FE9a4bbcAf0": 1600000
     }
     boosted = rewards_manager.boost_sett(sett, Snapshot(sett, balances, ratio=1, type=BalanceType.NonNative))
     print(boosted)
-    print(boosted_bals)
     TestCase().assertDictEqual(d1=boosted.balances, d2=boosted_bals)
     
 
