d708cb7c4529739933998aa881a1e092b32780b8
mitche50 <mitche50@gmail.com>
2021-11-09 16:10:26 -0500

    feat: add boost sanity check script

---
+++ scripts/prod_stag_sanity_check.py
@@ -0,0 +1,71 @@
+import json
+from math import isclose
+import os
+import random
+
+import boto3
+from helpers.enums import Network
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def download_boosts(chain: str, bucket: str):
+    s3 = boto3.client(
+            "s3",
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        )
+
+
+    print("DOWNLOADING BOOST")
+    chain_ids = {
+        Network.Ethereum: 1,
+        Network.Arbitrum: 42161,
+        Network.Polygon: 137,
+    }
+    chain_id = chain_ids[chain]
+
+    boost_file_name = f"badger-boosts-{chain_id}.json"
+    s3ClientObj = s3.get_object(Bucket=bucket, Key=boost_file_name)
+    data = json.loads(s3ClientObj["Body"].read().decode("utf-8"))
+    return data
+
+supported_chains = [Network.Ethereum, Network.Arbitrum, Network.Polygon]
+for chain in supported_chains:
+        
+    prod_boosts = download_boosts(chain, 'badger-merkle-proofs')
+    stag_boosts = download_boosts(chain, 'badger-staging-merkle-proofs')
+
+    assert len(prod_boosts['userData'].keys()) == len(stag_boosts['userData'].keys()) 
+
+    sample = random.sample(prod_boosts['userData'].keys(), 20)
+
+    for addr in sample:
+        prod_boost = prod_boosts['userData'][addr]['boost']
+        prod_native = prod_boosts['userData'][addr]['nativeBalance']
+        prod_non_native = prod_boosts['userData'][addr]['nonNativeBalance']
+        prod_stake_ratio = prod_boosts['userData'][addr]['stakeRatio']
+
+        stag_boost = stag_boosts['userData'][addr]['boost']
+        stag_native = stag_boosts['userData'][addr]['nativeBalance']
+        stag_non_native = stag_boosts['userData'][addr]['nonNativeBalance']
+        stag_stake_ratio = stag_boosts['userData'][addr]['stakeRatio']
+
+        boost_is_eq = isclose(prod_boost, stag_boost, abs_tol=1e-2)
+        native_is_eq = isclose(prod_native, stag_native, abs_tol=1e-2)
+        non_native_is_eq = isclose(prod_non_native, stag_non_native, abs_tol=1e-2)
+        stake_is_eq = isclose(prod_stake_ratio, stag_stake_ratio, abs_tol=1e-2)
+
+        print(f'Address {addr}:')
+        print(f'Boost is approximately equal: {boost_is_eq}')
+        if(not boost_is_eq):
+            print(f'{prod_boost} != {stag_boost}')
+        print(f'Native Balance is approximately equal: {native_is_eq}')
+        if(not native_is_eq):
+            print(f'{prod_native} != {stag_native}')
+        print(f'Non Native Balance is approximately equal: {non_native_is_eq}')
+        if(not non_native_is_eq):
+            print(f'{prod_non_native} != {stag_non_native}')
+        print(f'Stake Ratio is approximately equal: {stake_is_eq}')
+        if(not stake_is_eq):
+            print(f'{prod_stake_ratio} != {stag_stake_ratio}')
