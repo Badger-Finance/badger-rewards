e2bb6198f46f47cd84cf016ac3186c5c491cab7e
Darruma <umerderr@gmail.com>
2021-11-15 20:51:11 +0000

    chore: fix key name

--- rewards/emission_handlers.py
+++ rewards/emission_handlers.py
@@ -6,7 +6,7 @@ from rewards.utils.rewards_utils import distribute_rewards_to_snapshot
 
 def eth_tree_handler(amount: float, token: str, sett: str):
     claims = claims_snapshot(Network.Ethereum)
-    BCVXCRV = SETTS[Network.Ethereum]["bcvx_crv"]
+    BCVXCRV = SETTS[Network.Ethereum]["cvx_crv"]
     BVECVX = SETTS[Network.Ethereum]["bvecvx"]
     if sett in [BCVXCRV, BVECVX]:
         return distribute_rewards_to_snapshot(amount, claims[sett], token)
