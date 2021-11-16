48dcb33494339f431b5228779bc4c113ceec71d4
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-11-16 13:24:21 -0500

    chore: add test for fetch unboosted setts

--- tests/test_emission_utils.py
+++ tests/test_emission_utils.py
@@ -2,10 +2,31 @@ import pytest
 
 from helpers.constants import SETTS
 from helpers.enums import Network
-from rewards.utils.emission_utils import get_flat_emission_rate, get_token_weight
 from tests.utils import set_env_vars
 
 set_env_vars()
+
+from rewards.utils.emission_utils import (
+    fetch_setts,
+    fetch_unboosted_vaults,
+    get_flat_emission_rate,
+    get_token_weight,
+)
+
+
+class MockTime:
+    def __init__(self):
+        pass
+    def time(self):
+        return 1637086012
+
+
+def mock_fetch_setts(*args, **kwargs):
+    return [
+        SETTS[Network.Ethereum]["ren_crv"],
+        SETTS[Network.Ethereum]["sbtc_crv"],
+        SETTS[Network.Ethereum]["cvx"]
+    ]
 
 
 def test_get_flat_emission_rate():
@@ -33,3 +54,18 @@ def test_get_token_weight():
     for network, cases in all_cases.items():
         for token, weight in cases.items():
             assert get_token_weight(token, network) == weight
+
+
+def test_fetch_unboosted_vaults(monkeypatch):
+    monkeypatch.setattr(
+        "rewards.utils.emission_utils.fetch_setts", mock_fetch_setts
+    )
+    mock_time = MockTime()
+    monkeypatch.setattr(
+        "rewards.utils.emission_utils.time", mock_time
+    )
+    unboosted_vaults = [
+        SETTS[Network.Ethereum]["cvx"]
+    ]
+    vaults = fetch_unboosted_vaults(Network.Ethereum)
+    assert vaults == unboosted_vaults
