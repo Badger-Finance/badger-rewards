d6624a3d24be8503bf1017f958dd1289b94d9332
root <root@vmi480174.contaboserver.net>
2021-11-19 19:46:14 -0600

    feat: update docker data

--- scripts/rewards/eth/auto_approve_eth.py
+++ scripts/rewards/eth/auto_approve_eth.py
@@ -4,13 +4,15 @@ from rich.console import Console
 
 from helpers.enums import Network
 from scripts.rewards.eth.approve_eth import approve_rewards
+from config.singletons import env_config
 
 console = Console()
 
 if __name__ == "__main__":
+    kube = env_config.kube
     while True:
         try:
-            approve_rewards(Network.Ethereum)
+            approve_rewards(Network.Ethereum, kube)
         except Exception as e:
             console.print("[red]Error[/red]", e)
         finally:
