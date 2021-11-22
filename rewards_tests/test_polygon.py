6a3d88b3b4b44c0d1adf73f1113fe172dd4b8bfd
Andrew Kulikov <jadecoresky@gmail.com>
2021-11-22 19:51:13 +0200

    Completely remove rewards_tests modules as it's not used; Add init files to all python modules in the project

--- rewards_tests/test_polygon.py
+++
@@ -1,9 +0,0 @@
-from tests.utils import set_env_vars
-
-set_env_vars()
-
-from helpers.enums import Network
-from rewards_tests.test_propose_approve_rewards import test_propose_approve_rewards
-
-if __name__ == "__main__":
-    test_propose_approve_rewards(Network.Polygon)
