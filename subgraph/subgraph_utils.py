dbb5984e496b27f43be0b2b409f49b5f0ca66538
Swole Badger <swole@badger.finance>
2021-12-13 17:50:43 +0000

    feat: add seperate subgraphs urls for staging and prod

--- subgraph/subgraph_utils.py
+++ subgraph/subgraph_utils.py
@@ -1,22 +1,21 @@
+from re import sub
 from typing import Optional
 
 from gql import Client
 from gql.transport.requests import RequestsHTTPTransport
 
 from config.singletons import env_config
-from subgraph.config import subgraph_ids, subgraph_urls
+from helpers.enums import Environment
+from subgraph.config import subgraph_urls
 
 
 def subgraph_url(name: str) -> str:
-    if name in subgraph_ids:
-        api_key = env_config.graph_api_key
-        subgraph_id = subgraph_ids[name]
-        return f"https://gateway.thegraph.com/api/{api_key}/subgraphs/id/{subgraph_id}"
-    elif name in subgraph_urls:
-        return subgraph_urls[name]
+    prod_urls = subgraph_urls[Environment.Production]
+    staging_urls = subgraph_urls[Environment.Staging]
+    if env_config.production:
+        return prod_urls.get(name, "")
     else:
-        return ""
-
+        return staging_urls.get(name, prod_urls.get(name, ""))
 
 def make_gql_client(name: str) -> Optional[Client]:
     url = subgraph_url(name)
