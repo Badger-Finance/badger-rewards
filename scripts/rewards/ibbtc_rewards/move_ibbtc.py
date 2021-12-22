b2e56c7a8904c7b7302e7e3476885b4f4da02c11
Swole Badger <swole@badger.finance>
2021-12-22 21:02:05 +0000

    feat: use cvx from constants file

--- scripts/rewards/ibbtc_rewards/move_ibbtc.py
+++ scripts/rewards/ibbtc_rewards/move_ibbtc.py
@@ -1,8 +1,7 @@
-import requests
 from eth_utils.hexadecimal import encode_hex
 from web3 import Web3
 
-from helpers.constants import CHAIN_IDS, IBBTC_PEAK, TECH_OPS
+from helpers.constants import CHAIN_IDS, IBBTC_PEAK, SETTS, TECH_OPS
 from helpers.enums import Network
 from rewards.classes.MerkleTree import rewards_to_merkle_tree
 from rewards.classes.TreeManager import TreeManager
@@ -16,10 +15,9 @@ from rewards.utils.rewards_utils import (
 def move_ibbtc(tree, tree_manager: TreeManager):
     chain = Network.Ethereum
     rewards_list = merkle_tree_to_rewards_list(tree)
-    BCVXCRV = "0x2B5455aac8d64C14786c3a29858E43b5945819C0"
-    BCVX = "0x53C8E199eb2Cb7c01543C137078a038937a68E40"
+    BCVX = SETTS[Network.Ethereum]["cvx"]
     peak_data = tree["claims"][IBBTC_PEAK]
-    claimed_for = tree_manager.get_claimed_for(IBBTC_PEAK, [BCVX, BCVXCRV])
+    claimed_for = tree_manager.get_claimed_for(IBBTC_PEAK, [BCVX])
     bcvx_claimed_for = get_claimed_for_token(claimed_for, BCVX)
     bcvx_cumulative_amount = get_cumulative_claimable_for_token(peak_data, BCVX)
 
@@ -40,7 +38,6 @@ def move_ibbtc(tree, tree_manager: TreeManager):
     merkle_tree = rewards_to_merkle_tree(rewards_list, start_block, end_block)
     pre_tech_ops_bcvx = get_cumulative_claimable_for_token(tree["claims"][TECH_OPS], BCVX)
     post_tech_ops_bcvx = get_cumulative_claimable_for_token(merkle_tree["claims"][TECH_OPS], BCVX)
-    
     # Make sure total rewards don't change
     assert merkle_tree["tokenTotals"][BCVX] == tree["tokenTotals"][BCVX]
     assert bcvx_cumulative_amount == post_tech_ops_bcvx - pre_tech_ops_bcvx
