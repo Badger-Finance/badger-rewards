feb5f14279a75bd62f1c62b1c0ae70f61127a4b3
Swole Badger <swole@badger.finance>
2021-12-22 15:55:22 +0000

    feat: fetch correct and add extra sanity check

--- scripts/rewards/ibbtc_rewards/move_ibbtc.py
+++ scripts/rewards/ibbtc_rewards/move_ibbtc.py
@@ -8,6 +8,7 @@ from rewards.classes.MerkleTree import rewards_to_merkle_tree
 from rewards.classes.TreeManager import TreeManager
 from rewards.utils.rewards_utils import (
     get_claimed_for_token,
+    get_cumulative_claimable_for_token,
     merkle_tree_to_rewards_list,
 )
 
@@ -17,15 +18,11 @@ def move_ibbtc(tree, tree_manager: TreeManager):
     rewards_list = merkle_tree_to_rewards_list(tree)
     BCVXCRV = "0x2B5455aac8d64C14786c3a29858E43b5945819C0"
     BCVX = "0x53C8E199eb2Cb7c01543C137078a038937a68E40"
-
-    peak_data = requests.get(f"https://api.badger.com/v2/reward/tree/{IBBTC_PEAK}").json()
+    peak_data = tree["claims"][IBBTC_PEAK]
     claimed_for = tree_manager.get_claimed_for(IBBTC_PEAK, [BCVX, BCVXCRV])
-    bcvxcrv_claimed_for = get_claimed_for_token(claimed_for, BCVXCRV)
-    bcvxcrv_cumulative_amount = peak_data["cumulativeAmounts"][peak_data["tokens"].index(BCVXCRV)] 
     bcvx_claimed_for = get_claimed_for_token(claimed_for, BCVX)
-    bcvx_cumulative_amount = peak_data["cumulativeAmounts"][peak_data["tokens"].index(BCVX)]
+    bcvx_cumulative_amount = get_cumulative_claimable_for_token(peak_data, BCVX)
 
-    bcvxcrv_rewards = bcvxcrv_cumulative_amount - bcvxcrv_claimed_for
     bcvx_rewards = bcvx_cumulative_amount - bcvx_claimed_for
 
     rewards_list.increase_user_rewards(
@@ -41,9 +38,12 @@ def move_ibbtc(tree, tree_manager: TreeManager):
     start_block = int(tree["endBlock"]) + 1
     end_block = start_block
     merkle_tree = rewards_to_merkle_tree(rewards_list, start_block, end_block)
+    pre_tech_ops_bcvx = get_cumulative_claimable_for_token(tree["claims"][TECH_OPS], BCVX)
+    post_tech_ops_bcvx = get_cumulative_claimable_for_token(merkle_tree["claims"][TECH_OPS], BCVX)
+    
     # Make sure total rewards don't change
     assert merkle_tree["tokenTotals"][BCVX] == tree["tokenTotals"][BCVX]
-    assert merkle_tree["tokenTotals"][BCVXCRV] == tree["tokenTotals"][BCVXCRV]
+    assert bcvx_cumulative_amount == post_tech_ops_bcvx - pre_tech_ops_bcvx
 
     root_hash = Web3.keccak(text=merkle_tree["merkleRoot"])
     chain_id = CHAIN_IDS[chain]
