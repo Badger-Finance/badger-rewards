ad1bf0e39d1653d44fde051002a8ba5b62a05ef6
Swole Badger <swole@badger.finance>
2022-01-11 16:18:58 +0000

    feat: remove different staging subgraph£

--- subgraph/config.py
+++ subgraph/config.py
@@ -18,6 +18,5 @@ subgraph_urls = {
         "nfts": "https://api.thegraph.com/subgraphs/name/darruma/badger-nfts",
     },
     Environment.Staging: {
-        Network.Ethereum: "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-setts",
     },
 }
