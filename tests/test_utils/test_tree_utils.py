7637788baed943116c7a04e6814f9a5efcb4e245
Swole Badger <swole@badger.finance>
2022-01-11 18:49:39 +0000

    feat: remove duplicated fixture

--- tests/test_utils/test_tree_utils.py
+++ tests/test_utils/test_tree_utils.py
@@ -55,17 +55,7 @@ def test_get_last_proposed_cycle(tree_manager):
         )
     else:
         assert get_last_proposed_cycle(tree_manager.chain, tree_manager) == ({}, 0, 0)
-
-@pytest.fixture(scope="function")
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "us-west-1"
-
-
+        
 @pytest.mark.parametrize(
     "tree_manager",
     chains,
