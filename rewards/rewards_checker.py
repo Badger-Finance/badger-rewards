1af92af585cda1fe589435fe291eb9aa27e30e95
Andrew Kulikov <jadecoresky@gmail.com>
2022-01-14 12:04:55 +0200

    fix: style

--- rewards/rewards_checker.py
+++ rewards/rewards_checker.py
@@ -87,7 +87,9 @@ def verify_rewards(past_tree, new_tree, tree_manager: TreeManager, chain: str):
             total_before_token = digg_utils.shares_to_fragments(total_before_token)
             total_after_token = digg_utils.shares_to_fragments(total_after_token)
             decimals = 9
-        diff, table_item = token_diff_table_item(name, total_before_token, total_after_token, decimals)
+        diff, table_item = token_diff_table_item(
+            name, total_before_token, total_after_token, decimals
+        )
         table.append(table_item)
         send_code_block_to_discord(
             msg=tabulate(table, headers=["token", "before", "after", "diff"]),
