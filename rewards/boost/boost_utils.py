3c218c26f1e25794cdcb85b76033b86a1cc1192f
Swole Badger <swole@badger.finance>
2022-02-07 15:44:02 +0000

    feat: add bvecvx lp to boost calculation with correct ratio

--- rewards/boost/boost_utils.py
+++ rewards/boost/boost_utils.py
@@ -3,19 +3,21 @@ from dataclasses import dataclass
 from decimal import Decimal
 from typing import Dict, List, Tuple
 from rich.console import Console
-from helpers.enums import Network
+from helpers.web3_utils import make_contract
+from helpers.enums import Network, Abi
 import config.constants.addresses as addresses
 from rewards.snapshot.chain_snapshot import chain_snapshot_usd, sett_snapshot
 from rewards.snapshot.claims_snapshot import claims_snapshot, claims_snapshot_usd
 from rewards.snapshot.nft_snapshot import nft_snapshot_usd
 from rewards.snapshot.token_snapshot import token_snapshot_usd
 from rewards.classes.Boost import BoostBalances
-
 console = Console()
 
 
-dataclass
-
+def get_bvecvx_lp_ratio() -> float:
+    bvecvx_pool = make_contract(addresses.BVECVX_CVX_LP, Abi.Stableswap, Network.Ethereum)
+    balances = bvecvx_pool.get_balances()
+    return balances[1]/ sum(balances) # balances contains [cvx, bcvx]
 
 def calc_union_addresses(
     native_setts: Dict[str, float], non_native_setts: Dict[str, float]
@@ -48,13 +50,9 @@ def calc_boost_balances(block: int, chain: str) -> BoostBalances:
 
     native = Counter()
     non_native = Counter()
-
-    bvecvx_bals = sett_snapshot(chain, block, addresses.BVECVX, blacklist=True)
     console.log(f"\n === Taking claims snapshot on {chain} === \n")
     native_claimable, non_native_claimable = claims_snapshot_usd(chain, block)
     bvecvx_claimable = claims_snapshot(chain, block).get(addresses.BVECVX)
-
-    bvecvx_usd = (bvecvx_bals + bvecvx_claimable).convert_to_usd(chain).balances
     native += Counter(native_claimable)
     non_native += Counter(non_native_claimable)
     console.log(f"\n === Taking nft snapshot on {chain} === \n")
@@ -72,4 +70,14 @@ def calc_boost_balances(block: int, chain: str) -> BoostBalances:
 
     native = filter_dust(dict(native), 1)
     non_native = filter_dust(dict(non_native), 1)
+    bvecvx_usd = {}
+    if chain == Network.Ethereum:
+        bvecvx_claimable = claims_snapshot(chain, block).get(addresses.BVECVX)
+        bvecvx_bals = sett_snapshot(chain, block, addresses.BVECVX, blacklist=True)
+        bvecvx_lp_bals = sett_snapshot(chain, block, addresses.BVECVX_CVX_LP, blacklist=True)
+        ratio = get_bvecvx_lp_ratio()
+        for addr, value in bvecvx_bals:
+            bvecvx_lp_bals.boost_balance(addr, ratio)
+        bvecvx_usd = (bvecvx_bals + bvecvx_claimable + bvecvx_lp_bals).convert_to_usd(chain).balances
+
     return BoostBalances(native, non_native, bvecvx_usd, nft_balances)
