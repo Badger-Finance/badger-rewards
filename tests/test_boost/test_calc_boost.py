e831d688dc4463d565c93d40fe43a7a1e34f3ae9
Swole Badger <swole@badger.finance>
2022-02-08 14:48:53 +0000

    chore: close parenthesis

--- tests/test_boost/test_calc_boost.py
+++ tests/test_boost/test_calc_boost.py
@@ -25,7 +25,7 @@ def mock_discord_send_code(mocker):
 def test_calc_stake_ratio__happy():
     target = {"0x0000000000007F150Bd6f54c40A34d7C3d5e9f56": 0.1}
     assert (
-        calc_stake_ratio(TEST_USER, BoostBalances(target, target)
+        calc_stake_ratio(TEST_USER, BoostBalances(target, target))
         == list(target.values())[0] / list(target.values())[0]
     )
 
