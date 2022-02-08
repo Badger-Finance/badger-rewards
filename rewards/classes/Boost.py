b91b29009a01db6d8ae1fa46f46803e634d08a75
Swole Badger <swole@badger.finance>
2022-02-08 15:15:11 +0000

    feat: add default param for boost balances fields

--- rewards/classes/Boost.py
+++ rewards/classes/Boost.py
@@ -1,12 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict, Optional
 from decimal import Decimal
-
 
 @dataclass
 class BoostBalances:
     native: Dict[str, Decimal]
     non_native: Dict[str, Decimal]
-    bvecvx: Optional[Dict[str, Decimal]]
-    nfts: Optional[Dict[str, Decimal]]
+    bvecvx: Dict[str, Decimal] = field(default_factory=lambda: {})
+    nfts: Dict[str, Decimal] = field(default_factory=lambda: {})
+
+
 
