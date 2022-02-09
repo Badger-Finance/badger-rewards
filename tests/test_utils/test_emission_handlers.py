c694ec75c080318321432a7538630f620afddf41
Swole Badger <swole@badger.finance>
2022-02-09 15:19:35 +0000

    feat: remove unneeded test

--- tests/test_utils/test_emission_handlers.py
+++ tests/test_utils/test_emission_handlers.py
@@ -6,19 +6,6 @@ from dotmap import DotMap
 from rewards.emission_handlers import bvecvx_lp_handler
 
 
-def test_bvecvx_lp_handler__badger_no_rewards(mocker):
-    snapshot = mocker.patch(
-        "rewards.emission_handlers.sett_snapshot",
-        return_value=None
-    )
-    rewards = bvecvx_lp_handler(123, BADGER, "", 123)
-    assert rewards.claims == DotMap()
-    assert rewards.totals == DotMap()
-    assert rewards.cycle == 0
-
-    assert snapshot.called
-
-
 def test_bvecvx_lp_handler(mocker):
     mocker.patch(
         "rewards.emission_handlers.sett_snapshot",
