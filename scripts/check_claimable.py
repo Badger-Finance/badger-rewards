96a16426c1b30b36a11a9aaf195ab5177f8cb930
mitche50 <mitche50@gmail.com>
2022-02-10 09:29:37 -0500

    chore: update claimable check script

--- scripts/check_claimable.py
+++ scripts/check_claimable.py
@@ -1,7 +1,6 @@
 from rewards.snapshot.claims_snapshot import claims_snapshot
 from subgraph.queries.setts import last_synced_block
-from badger_api.requests import fetch_token
-from helpers.constants import ETH_BADGER_TREE
+from config.constants.addresses import ETH_BADGER_TREE
 from helpers.web3_utils import make_token
 import json
 from rich.console import Console
@@ -15,13 +14,19 @@ if __name__ == "__main__":
 
     console.log("claim snapshot taken ")
 
+    deficit = {}
+
     for token, snap in snapshots.items():
         t = make_token(token, chain)
         name = t.name().call()
-        tree_balance = t.balanceOf(ETH_BADGER_TREE).call() / 1e18
+        decimals = t.decimals().call()
+        tree_balance = t.balanceOf(ETH_BADGER_TREE).call() / (10 ** int(decimals))
         total = float(snap.total_balance())
+        deficit[name] = tree_balance - total
         console.log(name)
         console.log("Tree Balance: {}".format(tree_balance))
         console.log("Claimable Balance: {}".format(total))
         console.log("Deficit: {}".format(tree_balance - total))
         console.log("\n")
+
+    print(json.dumps(deficit, indent=4, sort_keys=True))
