b29af68dff258f96d36bde3b1148b050ca6bd76a
Swole Badger <swole@badger.finance>
2022-02-11 19:47:55 +0000

    feat: remove migration scripts

--- scripts/rewards/migration_rewards/approve.py
+++
@@ -1,6 +0,0 @@
-from scripts.rewards.utils.custom_cycle import custom_eth_approve
-from helpers.enums import Network
-from scripts.rewards.migration_rewards.migration import migration_rewards
-if __name__ == "__main__":
-    custom_eth_approve(Network.Ethereum, migration_rewards)
-
--- scripts/rewards/migration_rewards/propose.py
+++
@@ -1,6 +0,0 @@
-from scripts.rewards.utils.custom_cycle import custom_propose
-from helpers.enums import Network
-from scripts.rewards.migration_rewards.migration import migration_rewards
-if __name__ == "__main__":
-    custom_propose(Network.Ethereum, migration_rewards)
-
