1813b0f6f333746c49277300997077573549712f
Swole Badger <swole@badger.finance>
2022-02-14 20:50:48 +0000

    feat: fix import path

--- scripts/rewards/move_bvecvx/propose.py
+++ scripts/rewards/move_bvecvx/propose.py
@@ -1,7 +1,7 @@
 from scripts.rewards.utils.custom_cycle import custom_propose
 from helpers.enums import Network
-from scripts.rewards.move_bvecvx import move_bvecvx
+from scripts.rewards.move_bvecvx.move_bvecvx import move_bvecvx_func
 if __name__ == "__main__":
-    custom_propose(Network.Ethereum, move_bvecvx)
+    custom_propose(Network.Ethereum, move_bvecvx_func)
 
 
