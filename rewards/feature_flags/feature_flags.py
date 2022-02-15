9a840061f39285fcbdbb3a65c1b6406f03a61485
Andrew Kulikov <jadecoresky@gmail.com>
2022-02-15 14:44:54 +0200

    feature: wip on flag class

--- rewards/feature_flags/feature_flags.py
+++ rewards/feature_flags/feature_flags.py
@@ -1,7 +1,26 @@
+from typing import Dict
+
+
+class FlagNotFoundException(Exception):
+    pass
+
+
 class FeatureFlags:
-    FLAGS = {
-        "TEST_FLAG": True
+    """
+    Add new flags to FLAGS class attribute
+    Flag format should be {str: bool}
+    """
+    FLAGS: Dict[str, bool] = {
+        "TEST_FLAG_ENABLED": True
     }
-    
+
+    def does_flag_exist(self, flag: str) -> bool:
+        return bool(self.FLAGS.get(flag))
+
+    def flag_enabled(self, flag: str) -> bool:
+        if not self.does_flag_exist(flag):
+            raise FlagNotFoundException()
+        return self.FLAGS[flag]
+
 
 flags = FeatureFlags()
