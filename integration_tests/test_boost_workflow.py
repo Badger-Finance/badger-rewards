fc1a9c308cec938a28041a2ce0d4c3eba096a310
Andrew Kulikov <jadecoresky@gmail.com>
2022-02-16 19:10:45 +0200

    chore: mocking get_claimable_balances as well

--- integration_tests/test_boost_workflow.py
+++ integration_tests/test_boost_workflow.py
@@ -44,9 +44,35 @@ def test_boost_workflow(monkeypatch):
     monkeypatch.setattr(
         "badger_api.claimable.get_claimable_metadata",
         lambda *args, **kwargs:
-        {'chain': 'ethereum', 'startBlock': Decimal('14217430'), 'endBlock': Decimal('14217974'),
+        {'chain': 'lmao', 'startBlock': Decimal('14217430'), 'endBlock': Decimal('14217974'),
          'cycle': Decimal('4348'), 'chainStartBlock': 'ethereum_14217430'}
     )
+    monkeypatch.setattr(
+        "badger_api.claimable.get_claimable_balances",
+        lambda *args, **kwargs:
+        [{'claimableBalances': [], 'chainStartBlock': 'ethereum_14217430',
+          'expiresAt': Decimal('16  45112169.323'), 'chain': 'ethereum',
+          'startBlock': Decimal('14217430'),
+          'address': '0x0000000000000000000000000000000000000000'}, {
+             'claimableBalances': [{'balance': '417843248432115',
+                                    'address': '0x3472A5A71965499acd81997a54BBA8D852C6E53d'}],
+             'chainStartBlock': 'ethereum_14217430', 'expiresAt': Decimal('1645112169.324'),
+             'chain': 'ethereum', 'startBlock': Decimal('14217430'),
+             'address': '0x000000000000000000000000000000000000dEaD'},
+         {'claimableBalances': [], 'chainStartBlock': 'ethereum_14217430',
+          'expiresAt': Decimal('1645112169.324'), 'chain': 'ethereum',
+          'startBlock': Decimal('14217430'),
+          'address': '0x000000000000006F6502B7F2bbaC8C30A3f67E9a'},
+         {'claimableBalances': [], 'chainStartBlock': 'ethereum_14217430',
+          'expiresAt': Decimal('1645112169.324'), 'chain': 'ethereum',
+          'startBlock': Decimal('14217430'),
+          'address': '0x0000000000002D534FF79e9C69e7Fcc742f0BE83'},
+         {'claimableBalances': [], 'chainStartBlock': 'ethereum_14217430',
+          'expiresAt': Decimal('1645112169.324'), 'chain': 'ethereum',
+          'startBlock': Decimal('14217430'),
+          'address': '0x0000000000005117Dd3A72E64a705198753FDD54'}]
+    )
+
     current_block = env_config.get_web3().eth.block_number
     user_data = badger_boost(current_block, Network.Ethereum)
     # mock upload boosts has asserts baked in to check nothing Decimal,
