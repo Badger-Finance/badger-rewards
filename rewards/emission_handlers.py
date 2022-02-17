0042842877616efd5c0dd301351a9cff1efc7a7d
Swole Badger <swole@badger.finance>
2022-02-17 12:27:11 +0000

    feat: add handler for sending rewards to treasury handler

--- rewards/emission_handlers.py
+++ rewards/emission_handlers.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from rich.console import Console
 
-from config.constants.addresses import IBBTC_MULTISIG
+import config.constants.addresses as addresses
 from config.constants.chain_mappings import UNCLAIMED_REWARDS_TOKENS
 from helpers.enums import Network
 from rewards.classes.RewardsList import RewardsList
@@ -24,5 +24,12 @@ def unclaimed_rewards_handler(amount: Decimal, token: str, sett: str, block: int
 def ibbtc_peak_handler(amount: Decimal, token: str, sett: str, block: int) -> RewardsList:
     console.log("Distributing {} of {} to ibbtc multisig".format(amount, token))
     rewards = RewardsList()
-    rewards.increase_user_rewards(IBBTC_MULTISIG, token, amount)
+    rewards.increase_user_rewards(addresses.IBBTC_MULTISIG, token, amount)
+    return rewards
+
+
+def treasury_handler(amount: Decimal, token: str, sett: str, block: int) -> RewardsList:
+    console.log("Distributing {} of {} to treasruy multisig".format(amount, token))
+    rewards = RewardsList()
+    rewards.increase_user_rewards(addresses.TREASURY_OPS, token, amount)
     return rewards
