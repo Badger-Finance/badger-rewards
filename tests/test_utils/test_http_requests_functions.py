9f5a587599b019df2eeed2d8926cf8571e84b1a2
Andrew Kulikov <jadecoresky@gmail.com>
2022-02-18 12:18:14 +0200

    chore: cover with tests convert_from_eth func

--- tests/test_utils/test_http_requests_functions.py
+++ tests/test_utils/test_http_requests_functions.py
@@ -1,13 +1,16 @@
 import responses
 
-from badger_api.requests import (
-    badger_api,
-    fetch_ppfs,
-    fetch_token,
-    fetch_token_names,
-    fetch_token_prices,
-)
+from badger_api.requests import badger_api
+from badger_api.requests import fetch_ppfs
+from badger_api.requests import fetch_token
+from badger_api.requests import fetch_token_names
+from badger_api.requests import fetch_token_prices
+from config.constants import ARBITRUM_BLOCK_BUFFER
+from config.constants import POLYGON_BLOCK_BUFFER
 from config.constants.emissions import BOOST_CHAINS
+from helpers.enums import Network
+from rewards.explorer import CHAIN_EXPLORER_URLS
+from rewards.explorer import convert_from_eth
 from rewards.explorer import fetch_block_by_timestamp
 
 
@@ -58,6 +61,29 @@ def test_fetch_token_names_handled(mock_discord):
     responses.add(
         responses.GET, f"{badger_api}/tokens?chain=ethereum", json={}, status=404
     )
-    fetch_token_names("ethereum")
+    fetch_token_names(Network.Ethereum)
     assert mock_discord.call_count == 1
-    assert not fetch_token("ethereum", "token")
+    assert not fetch_token(Network.Ethereum, "token")
+
+
+@responses.activate
+def test_convert_from_eth(mock_discord):
+    block = 100000
+    timestamp = 1439799138
+    responses.add_passthru("https://")
+    for network in [Network.Ethereum, Network.Arbitrum, Network.Polygon]:
+        # static timestamp for ETH for block 100000
+        responses.add(
+            responses.GET,
+            f"https://api.{CHAIN_EXPLORER_URLS[network]}/api?module=block&action="
+            f"getblocknobytime&timestamp={timestamp}&closest=before&apikey=",
+            json={
+                'status': "1",
+                'result': f"{block}"
+            },
+            status=200,
+        )
+    data = convert_from_eth(block)
+    assert data[Network.Ethereum] == block
+    assert data[Network.Polygon] == block - POLYGON_BLOCK_BUFFER
+    assert data[Network.Arbitrum] == block - ARBITRUM_BLOCK_BUFFER
