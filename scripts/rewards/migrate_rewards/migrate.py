a0b10b7ca759e91c14ab6b6481c25a3f38d4b0cb
Darruma <30578602+Darruma@users.noreply.github.com>
2022-03-15 19:20:42 +0000

    Update migrate.py

--- scripts/rewards/migrate_rewards/migrate.py
+++ scripts/rewards/migrate_rewards/migrate.py
@@ -24,7 +24,7 @@ def migrate_func(tree, tree_manager) -> RewardsList:
             migration_harvest["token"],
             block
         ))
-    return combine_rewards([*migration_rewards, rewards_list], 4268)
+    return combine_rewards([*migration_rewards, rewards_list], 4267)
 
 
 def migrate_test(old_tree, new_tree):
