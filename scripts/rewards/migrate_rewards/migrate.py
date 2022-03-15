3078eaf9522eb5b2f2bea9593093574e73c40679
Swole Badger <swole@badger.finance>
2022-03-15 14:21:40 +0000

    feat: combine old rewards with new rewards

--- scripts/rewards/migrate_rewards/migrate.py
+++ scripts/rewards/migrate_rewards/migrate.py
@@ -11,7 +11,7 @@ from rewards.utils.rewards_utils import (
 
 
 def migrate_func(tree, tree_manager) -> RewardsList:
-    tree = merkle_tree_to_rewards_list(tree)
+    rewards_list = merkle_tree_to_rewards_list(tree)
     migration_rewards = []
     for migration_harvest in harvests:
         vault = migration_harvest["sett"]
@@ -19,21 +19,23 @@ def migrate_func(tree, tree_manager) -> RewardsList:
         amount = migration_harvest["amount"]
         snapshot = sett_snapshot(Network.Ethereum, block, vault)
         migration_rewards.append(distribute_rewards_from_total_snapshot(
-            amount,
+            int(amount),
             snapshot,
             migration_harvest["token"],
             block
         ))
-    return combine_rewards(migration_rewards, 0)
+    
+    return combine_rewards([*migration_rewards, rewards_list], 0)
 
 
 def migrate_test(old_tree, new_tree):
     token_agg = {}
     for migration_harvest in harvests:
         token = migration_harvest["token"]
-        token_agg[token] = token_agg.get(token) + migration_harvest["amount"]
+        token_agg[token] = token_agg.get(token, 0) + int(migration_harvest["amount"])
 
     for token, amount in old_tree["tokenTotals"].items():
-        if not amount == new_tree["tokenTotals"][token] + token_agg[token]:
+        new_amount = new_tree["tokenTotals"][token] + token_agg.get(token, 0)
+        if amount != new_amount:
             return False
     return True
