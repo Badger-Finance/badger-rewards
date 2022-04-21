b7c8ad498b789d8d23156ccda064404b705055b6
Swole Badger <swole@badger.finance>
2022-04-21 12:41:00 +0200

    feat: readd tests with boost step

--- tests/test_boost/test_calc_boost.py
+++ tests/test_boost/test_calc_boost.py
@@ -97,6 +97,9 @@ def test_get_badger_boost_data():
             "0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f7909": 0.9,
             "0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f7900": 0.2758,
             "0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f1111": 0,
+            "0x285C39e344179C253a75761C6737dE92183fA1F2": 1.25,
+            "0x0069f94C6Ef196cf54b2f0746dE92D40a83D41A5": 1.75,
+            "0x5E9F7E92e742F73b990dCa63c88325eD24666E84": 2.5
         }
     )
     assert data["0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f1111"] == 1
@@ -105,13 +108,16 @@ def test_get_badger_boost_data():
         assert data["0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f7900"] == 551
         assert data["0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f1111"] == 1
         assert data["0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f7909"] == math.floor(0.9 * 2000)
+        assert data["0x285C39e344179C253a75761C6737dE92183fA1F2"] == 2250
+        assert data["0x0069f94C6Ef196cf54b2f0746dE92D40a83D41A5"] == 2625
+        assert data["0x5E9F7E92e742F73b990dCa63c88325eD24666E84"] == 2875
 
     else:
         assert data["0x1f3e2aB8FE0C6E1f47acDcaa0b3B9db4044f7900"] == 500
-    assert data["0x0000000000007F150Bd6f54c40A34d7C3d5e9f56"] == 2000
-    assert stake_data[0] == 1
-    assert stake_data[0.9] == 1
-    assert stake_data[1] == 2
+        assert data["0x0000000000007F150Bd6f54c40A34d7C3d5e9f56"] == 2000
+        assert stake_data[0] == 1
+        assert stake_data[0.9] == 1
+        assert stake_data[1] == 5
 
 
 def test_badger_boost__happy(mock_discord_send_code, mock_snapshots, mocker, fetch_token_mock):
