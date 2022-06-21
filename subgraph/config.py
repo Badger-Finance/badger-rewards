17f5d7d9d7aa97695ab8a9b0738a801386240055
mitche50 <mitche50@gmail.com>
2022-06-21 08:36:51 -0400

    chore: use old subgraphs

--- subgraph/config.py
+++ subgraph/config.py
@@ -19,9 +19,6 @@ subgraph_urls = {
         Network.Fantom: (
             "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-setts-fantom"
         ),
-        Network.xDai: (
-            "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-setts-xdai"
-        ),
         # Token graphs
         f"tokens-{Network.Ethereum}": (
             "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-tokens"
@@ -33,14 +30,14 @@ subgraph_urls = {
             "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-tokens-polygon"
         ),
         f"tokens-{Network.Arbitrum}": (
-            "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-tokens-arbitrum"
+            "https://api.thegraph.com/subgraphs/name/darruma/badger-tokens-arbitrum"
         ),
         f"tokens-{Network.Fantom}": (
             "https://api.thegraph.com/subgraphs/name/badger-finance/badger-dao-tokens-fantom"
         ),
         "fuse": "https://api.thegraph.com/subgraphs/name/badger-finance/fuse-subgraph",
         "thegraph": "https://api.thegraph.com/index-node/graphql",
-        "nfts": "https://api.thegraph.com/subgraphs/name/badger-finance/badger-nfts",
+        "nfts": "https://api.thegraph.com/subgraphs/name/darruma/badger-nfts",
         "across": "https://api.thegraph.com/subgraphs/name/badger-finance/badger-across"
     },
     Environment.Staging: {
