6780a857964e7a22a80b657986dfd41f71ca76da
SHAKOTN <shakotn@protonmail.com>
2022-10-26 14:41:57 +0300

    feat: add custom cycle to redirect rewards

---
+++ scripts/rewards/redirect_rewards/propose.py
@@ -0,0 +1,13 @@
+import sys
+
+from helpers.enums import Network
+from logging_utils.logger import exception_logging
+from scripts.rewards.redirect_rewards.redirect_rewards import redirect_rewards_func
+from scripts.rewards.redirect_rewards.redirect_rewards import redirect_rewards_test
+from scripts.rewards.utils.custom_cycle import custom_propose
+
+sys.excepthook = exception_logging
+
+
+if __name__ == "__main__":
+    custom_propose(Network.Ethereum, redirect_rewards_func, redirect_rewards_test)
